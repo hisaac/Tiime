0243865e64b6e524d796802efe02b1362515f883
Isaac Halvorson <hello@hisaac.net>
2018-10-01 12:13:41 -0500

    Add Swiftgen goodness

---
+++ Tiime/Generated/Colors.swift
@@ -0,0 +1,88 @@
+// swiftlint:disable all
+// Generated using SwiftGen, by O.Halligon — https://github.com/SwiftGen/SwiftGen
+
+#if os(OSX)
+  import AppKit
+  internal enum ColorName { }
+#elseif os(iOS) || os(tvOS) || os(watchOS)
+  import UIKit
+  internal enum ColorName { }
+#endif
+
+// swiftlint:disable superfluous_disable_command
+// swiftlint:disable file_length
+
+// MARK: - Colors
+
+// swiftlint:disable identifier_name line_length type_body_length
+internal extension ColorName {
+  /// 0x216dbeff (r: 33, g: 109, b: 190, a: 255)
+  static let blue = #colorLiteral(red: 0.12941177, green: 0.42745098, blue: 0.74509805, alpha: 1.0)
+  /// 0x64a5ebff (r: 100, g: 165, b: 235, a: 255)
+  static let blueBackground = #colorLiteral(red: 0.39215687, green: 0.64705884, blue: 0.92156863, alpha: 1.0)
+  /// 0xcc6520ff (r: 204, g: 101, b: 32, a: 255)
+  static let brown = #colorLiteral(red: 0.8, green: 0.39607844, blue: 0.1254902, alpha: 1.0)
+  /// 0xd1783dff (r: 209, g: 120, b: 61, a: 255)
+  static let brownBackground = #colorLiteral(red: 0.81960785, green: 0.47058824, blue: 0.23921569, alpha: 1.0)
+  /// 0x6b706dff (r: 107, g: 112, b: 109, a: 255)
+  static let darkGray = #colorLiteral(red: 0.41960785, green: 0.4392157, blue: 0.42745098, alpha: 1.0)
+  /// 0x6b706dff (r: 107, g: 112, b: 109, a: 255)
+  static let darkGrayBackground = #colorLiteral(red: 0.41960785, green: 0.4392157, blue: 0.42745098, alpha: 1.0)
+  /// 0xcc2044ff (r: 204, g: 32, b: 68, a: 255)
+  static let darkPurple = #colorLiteral(red: 0.8, green: 0.1254902, blue: 0.26666668, alpha: 1.0)
+  /// 0xdb3d5eff (r: 219, g: 61, b: 94, a: 255)
+  static let darkPurpleBackground = #colorLiteral(red: 0.85882354, green: 0.23921569, blue: 0.36862746, alpha: 1.0)
+  /// 0xff9c00ff (r: 255, g: 156, b: 0, a: 255)
+  static let gold = #colorLiteral(red: 1.0, green: 0.6117647, blue: 0.0, alpha: 1.0)
+  /// 0xf5b929ff (r: 245, g: 185, b: 41, a: 255)
+  static let goldBackground = #colorLiteral(red: 0.9607843, green: 0.7254902, blue: 0.16078432, alpha: 1.0)
+  /// 0x28a422ff (r: 40, g: 164, b: 34, a: 255)
+  static let green = #colorLiteral(red: 0.15686275, green: 0.6431373, blue: 0.13333334, alpha: 1.0)
+  /// 0x50b833ff (r: 80, g: 184, b: 51, a: 255)
+  static let greenBackground = #colorLiteral(red: 0.3137255, green: 0.72156864, blue: 0.2, alpha: 1.0)
+  /// 0xd2d8d5ff (r: 210, g: 216, b: 213, a: 255)
+  static let lightGray = #colorLiteral(red: 0.8235294, green: 0.84705883, blue: 0.8352941, alpha: 1.0)
+  /// 0xd2d8d5ff (r: 210, g: 216, b: 213, a: 255)
+  static let lightGrayBackground = #colorLiteral(red: 0.8235294, green: 0.84705883, blue: 0.8352941, alpha: 1.0)
+  /// 0x8db115ff (r: 141, g: 177, b: 21, a: 255)
+  static let lime = #colorLiteral(red: 0.5529412, green: 0.69411767, blue: 0.08235294, alpha: 1.0)
+  /// 0x95bb1aff (r: 149, g: 187, b: 26, a: 255)
+  static let limeBackground = #colorLiteral(red: 0.58431375, green: 0.73333335, blue: 0.101960786, alpha: 1.0)
+  /// 0xcd4632ff (r: 205, g: 70, b: 50, a: 255)
+  static let maroon = #colorLiteral(red: 0.8039216, green: 0.27450982, blue: 0.19607843, alpha: 1.0)
+  /// 0xcd4632ff (r: 205, g: 70, b: 50, a: 255)
+  static let maroonBackground = #colorLiteral(red: 0.8039216, green: 0.27450982, blue: 0.19607843, alpha: 1.0)
+  /// 0x9aa39eff (r: 154, g: 163, b: 158, a: 255)
+  static let midGray = #colorLiteral(red: 0.6039216, green: 0.6392157, blue: 0.61960787, alpha: 1.0)
+  /// 0x9aa39eff (r: 154, g: 163, b: 158, a: 255)
+  static let midGrayBackground = #colorLiteral(red: 0.6039216, green: 0.6392157, blue: 0.61960787, alpha: 1.0)
+  /// 0xf76e00ff (r: 247, g: 110, b: 0, a: 255)
+  static let orange = #colorLiteral(red: 0.96862745, green: 0.43137255, blue: 0.0, alpha: 1.0)
+  /// 0xf08026ff (r: 240, g: 128, b: 38, a: 255)
+  static let orangeBackground = #colorLiteral(red: 0.9411765, green: 0.5019608, blue: 0.14901961, alpha: 1.0)
+  /// 0xe56bbeff (r: 229, g: 107, b: 190, a: 255)
+  static let pink = #colorLiteral(red: 0.8980392, green: 0.41960785, blue: 0.74509805, alpha: 1.0)
+  /// 0xf79ad9ff (r: 247, g: 154, b: 217, a: 255)
+  static let pinkBackground = #colorLiteral(red: 0.96862745, green: 0.6039216, blue: 0.8509804, alpha: 1.0)
+  /// 0x854fbbff (r: 133, g: 79, b: 187, a: 255)
+  static let purple = #colorLiteral(red: 0.52156866, green: 0.30980393, blue: 0.73333335, alpha: 1.0)
+  /// 0xb997dbff (r: 185, g: 151, b: 219, a: 255)
+  static let purpleBackground = #colorLiteral(red: 0.7254902, green: 0.5921569, blue: 0.85882354, alpha: 1.0)
+  /// 0xed5b3eff (r: 237, g: 91, b: 62, a: 255)
+  static let red = #colorLiteral(red: 0.92941177, green: 0.35686275, blue: 0.24313726, alpha: 1.0)
+  /// 0xed5b3eff (r: 237, g: 91, b: 62, a: 255)
+  static let redBackground = #colorLiteral(red: 0.92941177, green: 0.35686275, blue: 0.24313726, alpha: 1.0)
+  /// 0x00c0c2ff (r: 0, g: 192, b: 194, a: 255)
+  static let teal = #colorLiteral(red: 0.0, green: 0.7529412, blue: 0.7607843, alpha: 1.0)
+  /// 0x00c0c2ff (r: 0, g: 192, b: 194, a: 255)
+  static let tealBackground = #colorLiteral(red: 0.0, green: 0.7529412, blue: 0.7607843, alpha: 1.0)
+  /// 0xf1f5f3ff (r: 241, g: 245, b: 243, a: 255)
+  static let white = #colorLiteral(red: 0.94509804, green: 0.9607843, blue: 0.9529412, alpha: 1.0)
+  /// 0xf1f5f3ff (r: 241, g: 245, b: 243, a: 255)
+  static let whiteBackground = #colorLiteral(red: 0.94509804, green: 0.9607843, blue: 0.9529412, alpha: 1.0)
+  /// 0xe3bf14ff (r: 227, g: 191, b: 20, a: 255)
+  static let yellow = #colorLiteral(red: 0.8901961, green: 0.7490196, blue: 0.078431375, alpha: 1.0)
+  /// 0xf9f4c2ff (r: 249, g: 244, b: 194, a: 255)
+  static let yellowBackground = #colorLiteral(red: 0.9764706, green: 0.95686275, blue: 0.7607843, alpha: 1.0)
+}
+// swiftlint:enable identifier_name line_length type_body_length
