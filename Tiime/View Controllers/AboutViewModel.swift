bec96ec2b2e8e11b0302233edf9807ec0a107ee9
Isaac Halvorson <hello@hisaac.net>
2018-12-17 17:33:04 -0600

    Move some text to Localizable.strings

--- Tiime/View Controllers/AboutViewModel.swift
+++ Tiime/View Controllers/AboutViewModel.swift
@@ -1,7 +1,5 @@
 // Created by Isaac Halvorson on 12/15/18
 
-// swiftlint:disable line_length
-
 import os.log
 import UIKit
 
@@ -10,11 +8,10 @@ class AboutViewModel {
 	@objc var aboutCell: UITableViewCell {
 		let cell = UITableViewCell(style: .default, reuseIdentifier: #function)
 
-		cell.textLabel?.text = """
-			Thanks for checking out Tiime!
-
-			Tiime was born out of a fun programming challenge: To try to compute the current time in non-standard ways. After figuring that part out, I realized this would also be a great way to learn more about iOS development. I was especially interested in learning how to better develop user interfaces within Apple's guidelines and style.
-			"""
+		cell.textLabel?.text = NSLocalizedString(
+			"aboutScreenDescription",
+			comment: "About screen description"
+		)
 
 		cell.textLabel?.textColor = Theme.current.tableViewTextColor
 		cell.textLabel?.lineBreakMode = .byWordWrapping
@@ -26,10 +23,18 @@ class AboutViewModel {
 	@objc var codeCell: UITableViewCell {
 		let cell = UITableViewCell(style: .subtitle, reuseIdentifier: #function)
 
-		cell.textLabel?.text = "Tiime on GitHub"
+		cell.textLabel?.text = NSLocalizedString(
+			"Tiime on GitHub",
+			comment: "Title for GitHub link cell on About Screen"
+		)
+
 		cell.textLabel?.textColor = Theme.current.appTintColor
 
-		cell.detailTextLabel?.text = "Visit Tiime's GitHub project for more information, including technical information and implementation details. All the code is open source!"
+		cell.detailTextLabel?.text = NSLocalizedString(
+			"aboutScreenGitHubLinkDescription",
+			comment: "Description text for the link to Tiime's GitHub page"
+		)
+
 		cell.detailTextLabel?.lineBreakMode = .byWordWrapping
 		cell.detailTextLabel?.numberOfLines = 0
 		cell.detailTextLabel?.textColor = Theme.current.tableViewTextColor
@@ -45,7 +50,10 @@ class AboutViewModel {
 		cell.textLabel?.text = "@hisaac"
 		cell.textLabel?.textColor = Theme.current.appTintColor
 
-		cell.detailTextLabel?.text = "Follow the developer on Twitter"
+		cell.detailTextLabel?.text = NSLocalizedString(
+			"Follow the developer on Twitter",
+			comment: "Descriptive text for the link to the developer's Twitter account"
+		)
 		cell.detailTextLabel?.textColor = Theme.current.tableViewTextColor
 
 		cell.accessoryType = .disclosureIndicator
@@ -59,7 +67,10 @@ class AboutViewModel {
 		cell.textLabel?.text = "hisaac.net"
 		cell.textLabel?.textColor = Theme.current.appTintColor
 
-		cell.detailTextLabel?.text = "Visit the developer's website"
+		cell.detailTextLabel?.text = NSLocalizedString(
+			"Visit the developer's website",
+			comment: "Descriptive text for the link to the developer's website"
+		)
 		cell.detailTextLabel?.textColor = Theme.current.tableViewTextColor
 
 		cell.accessoryType = .disclosureIndicator
@@ -73,7 +84,10 @@ class AboutViewModel {
 		cell.textLabel?.text = "tiime@level.software"
 		cell.textLabel?.textColor = Theme.current.appTintColor
 
-		cell.detailTextLabel?.text = "Send feedback or ideas about the app"
+		cell.detailTextLabel?.text = NSLocalizedString(
+			"Send feedback or ideas about the app",
+			comment: "Descriptive text for the link to send the developer an email"
+		)
 		cell.detailTextLabel?.textColor = Theme.current.tableViewTextColor
 
 		cell.accessoryType = .disclosureIndicator
@@ -87,7 +101,10 @@ class AboutViewModel {
 		cell.textLabel?.text = "Ryan Lower"
 		cell.textLabel?.textColor = Theme.current.appTintColor
 
-		cell.detailTextLabel?.text = "Designed the app's beautiful icons"
+		cell.detailTextLabel?.text = NSLocalizedString(
+			"Designed the app's beautiful icons",
+			comment: "Descriptive text for the link to Ryan Lower's Twitter profile"
+		)
 		cell.detailTextLabel?.textColor = Theme.current.tableViewTextColor
 
 		cell.accessoryType = .disclosureIndicator
@@ -101,7 +118,10 @@ class AboutViewModel {
 		cell.textLabel?.text = "Eli Mellen"
 		cell.textLabel?.textColor = Theme.current.appTintColor
 
-		cell.detailTextLabel?.text = "QA testing and wicked helpful app feedback"
+		cell.detailTextLabel?.text = NSLocalizedString(
+			"QA testing and wicked helpful app feedback",
+			comment: "Descriptive text for the link to Eli Mellen's web page"
+		)
 		cell.detailTextLabel?.textColor = Theme.current.tableViewTextColor
 
 		cell.accessoryType = .disclosureIndicator
