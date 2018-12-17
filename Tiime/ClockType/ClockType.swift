29c869625c718c26bece2c329ec345b8b87fa4ad
Isaac Halvorson <hello@hisaac.net>
2018-12-17 17:35:04 -0600

    Move Device Time to the bottom of the list

--- Tiime/ClockType/ClockType.swift
+++ Tiime/ClockType/ClockType.swift
@@ -5,9 +5,9 @@ import Foundation
 enum ClockType: CaseIterable {
 
 	case beat   // Swatch .beat time / Internet Time
-	case device // The time displayed based on the device's setting
 	case unix   // Unix time / Epoch time
 	case metric // Metric Time
+	case device // The time displayed based on the device's setting
 
 }
 
@@ -17,9 +17,9 @@ extension ClockType {
 	var timeRepresentable: TimeRepresentable {
 		switch self {
 		case .beat:   return BeatTime()
-		case .device: return DeviceTime()
 		case .unix:   return UnixTime()
 		case .metric: return MetricTime()
+		case .device: return DeviceTime()
 		}
 	}
 
@@ -27,9 +27,9 @@ extension ClockType {
 	var name: String {
 		switch self {
 		case .beat:   return NSLocalizedString(".beat Time", comment: "Name of .beat Time")
-		case .device: return NSLocalizedString("Device Time", comment: "Name of Device Time")
 		case .unix:   return NSLocalizedString("Unix Time", comment: "Name of Unix Time")
 		case .metric: return NSLocalizedString("Metric Time", comment: "Name of Metric Time")
+		case .device: return NSLocalizedString("Device Time", comment: "Name of Device Time")
 		}
 	}
 
@@ -41,11 +41,6 @@ extension ClockType {
 				"The day is divided into 1000 \"beats\"",
 				comment: "Description for .beat Time"
 			)
-		case .device:
-			return NSLocalizedString(
-				"The time displayed in the default way for the device's locale",
-				comment: "Description for Device Time"
-			)
 		case .unix:
 			return NSLocalizedString(
 				"The amount of seconds since January 1, 1970",
@@ -55,6 +50,11 @@ extension ClockType {
 			return NSLocalizedString(
 				"The day is divided into units of tenths, hundredths, thousands, etc.",
 				comment: "Description for Metric Time"
+			)
+		case .device:
+			return NSLocalizedString(
+				"The time displayed in the default way for the device's locale",
+				comment: "Description for Device Time"
 			)
 		}
 	}
